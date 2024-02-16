5a6ebcf9c014d0ee6e50f2930d036bf82d37399d
Elena Petrova <helenofspb@gmail.com>
2024-02-16 13:59:17 +0300

    ASTONJS-1 [feat]: 2 vs git

--- example.js
+++ example.js
@@ -1,1 +1,20 @@
-let a = 3
+function getLength(value) {
+    if(value && value.length !== undefined) {
+        console.log(value.length)
+    }else{
+        console.log(0) 
+    }
+}
+getLength('6')
+getLength(4)
+getLength([1, 2, 6])
+
+
+function getLength(value) {
+    const valueLength = value && value.length !== undefined ? value.length : 0;
+    console.log(valueLength);
+}
+getLength('6')
+getLength(4)
+getLength([1, 2, 6])
+
